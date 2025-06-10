fa42b0804ca66296137e1a9508fd52c897171e51
angele-levq <angele.levesque@etu.estia.fr>
2025-06-10 10:34:48 +0200

    #20

--- src/features.c
+++ src/features.c
@@ -88,21 +88,33 @@ void tenth_pixel(char* filename) {
 }
 
 void max_pixel(char *filename){
-    int W, H, x, y, max_somme=-1, max_x=0, max_y=0, R, G, B, somme=mp(R+B+G), mp;
-    pixelRGB **image
-    read_image_data(filename, &image, &W, &H);
+    int W, H, x, y, max_somme=-1, max_x=0, max_y=0, channel_count;
+    unsigned char *data;
+
+    int result = read_image_data(filename, &data, &W, &H, &channel_count);
+    if (result == 0) {
+        printf("Erreur avec le fichier %s\n", filename);
+        return;
+    }
 
     for (y=0; y<H; y++) {
         for (x=0; x<W; x++) {
+            int index=(y*W+x)*channel_count;
+            int R, G, B;
+
+            R=data[index];
+            G=data[index + 1];
+            B=data[index + 2];
+
+            int somme=R+G+B;
 
             if (somme>max_somme){
                 max_somme=somme;
-                max_pixel=mp;
                 max_x=x;
                 max_y=y;
             }
         }
     }
-
-    printf("max_pixel (%d, %d): %d, %d, %d\n", max_x, max_y, mp.R, mp.G, mp.B)
+    int max_index=(max_y*W+max_x)*channel_count;
+    printf("max_pixel (%d, %d): %d, %d, %d\n", max_x, max_y, data[max_index], data[max_index + 1], data[max_index + 2]) ;
 }
